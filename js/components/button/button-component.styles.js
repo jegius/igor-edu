51152fd9099d5f19413f46b555eae131f3ec6f67
New account <newaccount@News-MacBook-Pro.local>
2023-07-15 17:38:41 +0300

    transition в стилях оставлен только у .button,

--- js/components/button/button-component.styles.js
+++ js/components/button/button-component.styles.js
@@ -6,7 +6,7 @@ export default `
             background: transparent;
             color: var(--orangeColor);
             cursor: pointer;
-            transition: background-color .3s ease-in, color .3s ease-out;
+            transition: background-color .3s ease-in, color .3s ease-out, transform .3s ease-in;
             padding: 0.75rem 2rem;
             border-radius: .625rem;
         }
@@ -14,14 +14,12 @@ export default `
         .button:hover {
             background-color: var(--orange);
             color: var(--white);
-            transition: .3s all;
             transform: scale(1.1);
         }
 
         ._active {
             background: var(--orange);
             color: var(--white);
-            transition: .3s all;
         }
 
         ._active:hover {
