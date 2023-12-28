0396e5ce087383630cd3cb459464e73f8330d640
NamerrorOne <igor14abramov@gmail.com>
2023-12-28 23:20:40 +0300

    fist version of custom scroll component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,13 @@
-import { NavComponent } from "../js/components/nav/nav-component.js";
-import { LinkComponent } from "../js/components/link/link-component.js";
-import { ButtonComponent } from "../js/components/button/button-component.js";
-import { LogoComponent } from "../js/components/logo/logo-component.js";
-import { TitleComponent } from "../js/components/title/title-component.js";
-import { ImageComponent } from "../js/components/image/image-component.js";
-import { HeaderComponent } from "../js/components/header/header-component.js";
-import { SectionComponent } from '../js/components/section/section-component.js';
+import { ButtonComponent } from '../js/components/button/button-component.js';
 import { CardComponent } from '../js/components/card/card-component.js';
+import { HeaderComponent } from '../js/components/header/header-component.js';
+import { ImageComponent } from '../js/components/image/image-component.js';
+import { LinkComponent } from '../js/components/link/link-component.js';
+import { LogoComponent } from '../js/components/logo/logo-component.js';
+import { NavComponent } from '../js/components/nav/nav-component.js';
+import { ScrollComponent } from '../js/components/scroll/scroll-component.js';
+import { SectionComponent } from '../js/components/section/section-component.js';
+import { TitleComponent } from '../js/components/title/title-component.js';
 
 /** @type { import('@storybook/web-components').Preview } */
 const preview = {
@@ -31,5 +32,6 @@ const preview = {
   HeaderComponent,
   SectionComponent,
   CardComponent,
+  ScrollComponent,
 ].map(component => customElements.define(component.name, component));
 export default preview;
--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-import { LinkComponent } from "./components/link/link-component.js";
-import { NavComponent } from "./components/nav/nav-component.js";
-import { ButtonComponent } from "./components/button/button-component.js";
-import { LogoComponent } from "./components/logo/logo-component.js";
-import { TitleComponent } from "./components/title/title-component.js";
-import { ImageComponent } from "./components/image/image-component.js";
-import { HeaderComponent } from "./components/header/header-component.js";
-import { SectionComponent } from './components/section/section-component.js';
+import { ButtonComponent } from './components/button/button-component.js';
 import { CardComponent } from './components/card/card-component.js';
-
+import { HeaderComponent } from './components/header/header-component.js';
+import { ImageComponent } from './components/image/image-component.js';
+import { LinkComponent } from './components/link/link-component.js';
+import { LogoComponent } from './components/logo/logo-component.js';
+import { NavComponent } from './components/nav/nav-component.js';
+import { ScrollComponent } from './components/scroll/scroll-component.js';
+import { SectionComponent } from './components/section/section-component.js';
+import { TitleComponent } from './components/title/title-component.js';
 [
   ButtonComponent,
   NavComponent,
@@ -16,6 +16,7 @@ import { CardComponent } from './components/card/card-component.js';
   TitleComponent,
   HeaderComponent,
   ImageComponent,
-  SectionComponent, 
-  CardComponent
+  SectionComponent,
+  CardComponent,
+  ScrollComponent,
 ].map(component => customElements.define(component.name, component));
